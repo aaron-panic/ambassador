945c3d841701b3575c4541d838ba6f93bdcd867d
Aaron Reichenbach <aaron@scavengers.io>
2026-02-24 13:54:38 -0700

    boilerplate visual layers

---
+++ src/visual_layer.hxx
@@ -0,0 +1,22 @@
+#ifndef VISUAL_LAYERS_HXX_INCLUDED
+#define VISUAL_LAYERS_HXX_INCLUDED
+
+#include "damb_format.hxx"
+
+class VisualLayer {
+
+};
+
+class MapLayer : public VisualLayer {
+
+};
+
+class SpriteLayer : public VisualLayer {
+
+};
+
+class EffectLayer : public VisualLayer {
+
+};
+
+#endif
