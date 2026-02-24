db6b03599ac1e7f5645a378d1f122b452cd19ccb
Aaron Reichenbach <aaron@scavengers.io>
2026-02-24 14:53:07 -0700

    runtime, redux

--- src/damb_runtime.hxx
+++ src/damb_runtime.hxx
@@ -1,6 +1,36 @@
 #ifndef DAMB_RUNTIME_HXX_INCLUDED
 #define DAMB_RUNTIME_HXX_INCLUDED
 
-// Runtime optimized containers for rendering
+#include "damb_format.hxx"
+#include "amb_types.hxx"
+#include <vector>
+#include <memory>
+#include <SDL3/SDL.h>
+
+
+struct TextureDeleter {
+    void operator()(SDL_Texture* t) { if (t) SDL_DestroyTexture(t); }
+};
+using TexturePtr = std::unique_ptr<SDL_Texture, TextureDeleter>;
+
+struct ImageRuntime {
+    TexturePtr texture;
+};
+
+struct AtlasRuntime {
+    std::vector<SDL_FRect> rects;
+};
+
+struct MapCellRuntime {
+    std::vector<u16> atlas_idx;
+};
+
+struct MapRuntime {
+    u32 width;
+    u32 height;
+    u16 tile_width;
+    u16 tile_height;
+    std::vector<MapCellRuntime> cells;
+}
 
 #endif
