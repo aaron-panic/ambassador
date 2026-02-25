6596545f74179d3e1eb48c5af8baf4dd6833bdc8
aaron-panic <105097900+aaron-panic@users.noreply.github.com>
2026-02-25 16:30:43 -0700

    Split config into dedicated ambconfig library

--- src/config.cxx
+++ src/config.cxx
@@ -1,16 +1,16 @@
 #include "config.hxx"
 
-const char* amb::config::APP_TITLE = "Ambassador";
-const char* amb::config::APP_VERSION = "0.1";
-const char* amb::config::APP_IDENTIFIER = "io.scavengers.ambassador";
+extern const char* amb::config::APP_TITLE = "Ambassador";
+extern const char* amb::config::APP_VERSION = "0.1";
+extern const char* amb::config::APP_IDENTIFIER = "io.scavengers.ambassador";
 
-const int amb::config::DEFAULT_APP_WIDTH = 800;
-const int amb::config::DEFAULT_APP_HEIGHT = 600;
+extern const int amb::config::DEFAULT_APP_WIDTH = 800;
+extern const int amb::config::DEFAULT_APP_HEIGHT = 600;
 
-const u64 amb::config::GAME_SPEED = 60;
-const u64 amb::config::UPDATE_SPEED = 1000 / GAME_SPEED;
+extern const u64 amb::config::GAME_SPEED = 60;
+extern const u64 amb::config::UPDATE_SPEED = 1000 / GAME_SPEED;
 
-const u8 amb::game::MAP_TILE_SIZE = 50;
+extern const u8 amb::game::MAP_TILE_SIZE = 50;
 
-const u8 amb::data::CHUNK_TYPE_LENGTH = 4;
-const u8 amb::data::MAGIC_LENGTH = 8;
+extern const u8 amb::data::CHUNK_TYPE_LENGTH = 4;
+extern const u8 amb::data::MAGIC_LENGTH = 8;
